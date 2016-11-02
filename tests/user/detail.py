fe080dd257694bf2345ce3127faeb99e629f9548
Nemec Lukas <lukas.nemec2@firma.seznam.cz>
2016-11-02 17:34:23 +0100

    Oprava testu po refaktoru.

--- tests/user/detail.py
+++ tests/user/detail.py
@@ -1,8 +1,26 @@
 import json
+import random
 from flask import url_for
 
 
 def test_get_one(client, user):
     result = client.get(url_for('user', user_id=user.id))
     json_data = json.loads(result.data.decode('utf-8'))
-    assert json_data == {'id': user.id, 'username': user.username, 'password_hash': user.password_hash}
+    assert json_data == {
+        'status_code': 200, 
+        'data': {'id': user.id, 'username': user.username, 'password_hash': user.password_hash}
+    }
+
+
+def test_not_found(client, user):
+    result = client.get(url_for('user', user_id=random.randint(10, 99999)))
+    json_data = json.loads(result.data.decode('utf-8'))
+    assert json_data == {'status_code': 404}
+
+
+def test_unspecified_error(client):
+    result = client.get(url_for('user', user_id=1))
+    json_data = json.loads(result.data.decode('utf-8'))
+
+    assert json_data['status_code'] == 500
+    assert 'info' in json_data
--- tests/user/listing.py
+++ tests/user/listing.py
@@ -5,11 +5,12 @@ from flask import url_for
 
 def test_list_empty(db, client):
     result = client.get(url_for('user_listing'))
-    assert json.loads(result.data.decode('utf-8')) == []
+    assert json.loads(result.data.decode('utf-8')) == {'status_code': 200, 'data': []}
 
 
 def test_list_one(client, user):
     result = client.get(url_for('user_listing'))
     json_data = json.loads(result.data.decode('utf-8'))
-    assert len(json_data) == 1
-    assert json_data[0]['username'] == user.username
+    assert json_data['status_code'] == 200
+    assert len(json_data['data']) == 1
+    assert json_data['data'][0]['username'] == user.username
