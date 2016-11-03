9817c58f41cdc29e17e92d7b3859f7376bbf2c7b
Dan Pressl <dan.pressl@firma.seznam.cz>
2016-11-03 15:16:32 +0100

    Pridani migraci, pocatecni migrace

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def register_db(app):    
     db.init_app(app)
+    migrate.init_app(app, db)
     # Alias pro snadny pristup.
     app.db = db
